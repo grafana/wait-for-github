bb4ecca8082affd74ecef8ae602d28920695b556
Iain Lane <iain.lane@grafana.com>
2023-06-23 14:11:15 +0100

    refactor part 2: Separate functions
    Add a `Check` interface. This lets us separate out the part which is
    repeatedly called into their own functions. Then they'll be able to be
    tested.

--- cmd/wait-for-github/main.go
+++ cmd/wait-for-github/main.go
@@ -1,5 +1,5 @@
 // wait-for-github
-// Copyright (C) 2022, Grafana Labs
+// Copyright (C) 2022-2023, Grafana Labs
 
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the GNU Affero General Public License as published by the Free
