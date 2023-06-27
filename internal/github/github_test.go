91144af938792734ad032cae815251ca42beee89
Iain Lane <iain.lane@grafana.com>
2023-06-27 12:19:41 +0100

    github: Add more testcases
    Now we've done all that refactoring, we can start to add more testcases
    sensibly. Actually, this particular commit would have been more or less
    possible already.
    
    Test the low-level github internal module. We've aimed for as close to
    100% coverage as we can get, so there are a lot of cases.

--- internal/github/github_test.go
+++ internal/github/github_test.go
@@ -19,6 +19,7 @@ package github
 import (
 	"context"
 	"net/http"
+	"net/url"
 	"testing"
 	"time"
 
@@ -35,6 +36,8 @@ import (
 // whose transport is correctly configured to use the provided token and uses a
 // retrying transport which itself uses a caching transport.
 func TestNewGithubClientWithToken(t *testing.T) {
+	t.Parallel()
+
 	ctx := context.Background()
 
 	// Set up test data
@@ -42,11 +45,8 @@ func TestNewGithubClientWithToken(t *testing.T) {
 		GithubToken: "my-token",
 	}
 
-	client, err := NewGithubClient(ctx, authInfo)
+	githubClient, err := NewGithubClient(ctx, authInfo)
 	require.NoError(t, err)
-
-	githubClient, ok := client.(*GHClient)
-	require.Truef(t, ok, "Returned client is not a GHClient (is %T)", client)
 
 	if githubClient.client == nil {
 		t.Fatal("Returned client has nil client field")
@@ -70,6 +70,8 @@ func TestNewGithubClientWithToken(t *testing.T) {
 // authentication and uses a retrying transport which itself uses a caching
 // transport.
 func TestNewGithubClientWithAppAuthentication(t *testing.T) {
+	t.Parallel()
+
 	ctx := context.Background()
 
 	// Set up test data
@@ -80,11 +82,8 @@ func TestNewGithubClientWithAppAuthentication(t *testing.T) {
 		PrivateKey: []byte("-----BEGIN RSA PRIVATE KEY-----\nMC0CAQACBQD7J5Q9AgMBAAECBB6C8NkCAwD+JwIDAPz7AgMA1xcCAkoZAgMAwE8=\n-----END RSA PRIVATE KEY-----"),
 	}
 
-	client, err := NewGithubClient(ctx, authInfo)
+	githubClient, err := NewGithubClient(ctx, authInfo)
 	require.NoError(t, err)
-
-	githubClient, ok := client.(*GHClient)
-	require.Truef(t, ok, "Returned client is not a GHClient (is %T)", client)
 
 	if githubClient.client == nil {
 		t.Fatal("Returned client has nil client field")
@@ -124,6 +123,8 @@ func newClientFromMock(t *testing.T, mockClient *http.Client) *GHClient {
 // TestResponsesAreRetried tests that responses are retried by the retrying
 // transport which underlies the github clients we create.
 func TestResponsesAreRetried(t *testing.T) {
+	t.Parallel()
+
 	retryCount := 0
 	mockClient := mock.NewMockedHTTPClient(
 		mock.WithRequestMatchHandler(
@@ -149,6 +150,8 @@ func TestResponsesAreRetried(t *testing.T) {
 // TestResponsesAreCached tests that responses are cached by the caching
 // transport which underlies the github clients we create.
 func TestResponsesAreCached(t *testing.T) {
+	t.Parallel()
+
 	hits := 0
 	lastStatusSent := 0
 
@@ -224,4 +227,420 @@ func TestResponsesAreCached(t *testing.T) {
 	require.Equal(t, sha, sha3)
 	require.Equal(t, closed, closed3)
 	require.Equal(t, mergedTs, mergedTs3)
+}
+
+func errorReturningHandler(t *testing.T, ep mock.EndpointPattern) mock.MockBackendOption {
+	t.Helper()
+
+	return mock.WithRequestMatchHandler(
+		mock.GetReposCommitsCheckRunsByOwnerByRepoByRef,
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			mock.WriteError(
+				w,
+				http.StatusInternalServerError,
+				"Internal Server Error",
+			)
+		}),
+	)
+}
+
+func newErrorReturningClient(t *testing.T) *GHClient {
+	t.Helper()
+
+	return newClientFromMock(t, mock.NewMockedHTTPClient(
+		errorReturningHandler(t, mock.GetReposCommitsCheckRunsByOwnerByRepoByRef),
+	))
+}
+
+func TestIsPRMergedOrClosed_Success(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+
+	mockedHTTPClient := mock.NewMockedHTTPClient(
+		mock.WithRequestMatch(
+			mock.GetReposPullsByOwnerByRepoByPullNumber,
+			github.PullRequest{
+				Number:         github.Int(1),
+				State:          github.String("closed"),
+				Merged:         github.Bool(true),
+				MergedAt:       &github.Timestamp{Time: time.Now()},
+				MergeCommitSHA: github.String("abcdef12345"),
+			},
+		),
+	)
+
+	ghClient := newClientFromMock(t, mockedHTTPClient)
+	sha, closed, mergedAt, err := ghClient.IsPRMergedOrClosed(ctx, "owner", "repo", 1)
+
+	require.NoError(t, err)
+	require.Equal(t, "abcdef12345", sha)
+	require.True(t, closed)
+	// As time is not frozen, we only check that mergedAt is not zero
+	require.NotZero(t, mergedAt)
+}
+
+func TestIsPRMergedOrClosed_Error(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+	ghClient := newErrorReturningClient(t)
+	_, _, _, err := ghClient.IsPRMergedOrClosed(ctx, "owner", "repo", 1)
+	require.Error(t, err)
+}
+
+func TestGetCIStatus(t *testing.T) {
+	tests := []struct {
+		name           string
+		status         *github.CombinedStatus
+		expectedStatus CIStatus
+	}{
+		{
+			name: "success",
+			status: &github.CombinedStatus{
+				State: github.String("success"),
+			},
+			expectedStatus: CIStatusPassed,
+		},
+		{
+			name: "pending",
+			status: &github.CombinedStatus{
+				State: github.String("pending"),
+			},
+			expectedStatus: CIStatusPending,
+		},
+		{
+			name: "failure",
+			status: &github.CombinedStatus{
+				State: github.String("failure"),
+			},
+			expectedStatus: CIStatusFailed,
+		},
+		{
+			name: "unknown",
+			status: &github.CombinedStatus{
+				State: github.String("unknown"),
+			},
+			expectedStatus: CIStatusUnknown,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			ctx := context.Background()
+
+			mockedHTTPClient := mock.NewMockedHTTPClient(
+				mock.WithRequestMatch(
+					mock.GetReposCommitsStatusByOwnerByRepoByRef,
+					tt.status,
+				),
+			)
+
+			ghClient := newClientFromMock(t, mockedHTTPClient)
+			result, err := ghClient.GetCIStatus(ctx, "owner", "repo", "abcdef12345")
+
+			require.NoError(t, err)
+			require.Equalf(t, tt.expectedStatus, result, "expected %s, got %s", tt.expectedStatus, result)
+		})
+	}
+}
+
+func TestGetCIStatus_Error(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+	ghClient := newErrorReturningClient(t)
+	_, err := ghClient.GetCIStatus(ctx, "owner", "repo", "abcdef12345")
+	require.Error(t, err)
+}
+
+func TestGetCIStatusForChecks(t *testing.T) {
+	tests := []struct {
+		name            string
+		checksToLookFor []string
+		mockCheckRuns   []*github.CheckRun
+		mockRepoStatus  []*github.RepoStatus
+		expectedStatus  CIStatus
+		expectedAwait   []string
+	}{
+		{
+			name:            "Single check - completed with success",
+			checksToLookFor: []string{"check1"},
+			mockCheckRuns: []*github.CheckRun{
+				{
+					Name:       github.String("check1"),
+					Status:     github.String("completed"),
+					Conclusion: github.String("success"),
+				},
+			},
+			expectedStatus: CIStatusPassed,
+			expectedAwait:  nil,
+		},
+		{
+			name:            "Single check - completed with failure",
+			checksToLookFor: []string{"check1"},
+			mockCheckRuns: []*github.CheckRun{
+				{
+					Name:       github.String("check1"),
+					Status:     github.String("completed"),
+					Conclusion: github.String("failure"),
+				},
+			},
+			expectedStatus: CIStatusFailed,
+			expectedAwait:  []string{"check1"},
+		},
+		{
+			name:            "Single check - queued",
+			checksToLookFor: []string{"check1"},
+			mockCheckRuns: []*github.CheckRun{
+				{
+					Name:   github.String("check1"),
+					Status: github.String("queued"),
+				},
+			},
+			expectedStatus: CIStatusPending,
+			expectedAwait:  []string{"check1"},
+		},
+		{
+			name:            "Single check - in progress",
+			checksToLookFor: []string{"check1"},
+			mockCheckRuns: []*github.CheckRun{
+				{
+					Name:   github.String("check1"),
+					Status: github.String("in_progress"),
+				},
+			},
+			expectedStatus: CIStatusPending,
+			expectedAwait:  []string{"check1"},
+		},
+		{
+			name:            "Single check - skipped",
+			checksToLookFor: []string{"check1"},
+			mockCheckRuns: []*github.CheckRun{
+				{
+					Name:       github.String("check1"),
+					Conclusion: github.String("skipped"),
+					Status:     github.String("completed"),
+				},
+			},
+			expectedStatus: CIStatusPassed,
+			expectedAwait:  nil,
+		},
+		{
+			name:            "Single check - doesn't exist, status success",
+			checksToLookFor: []string{"check1"},
+			mockRepoStatus: []*github.RepoStatus{
+				{
+					Context: github.String("check1"),
+					State:   github.String("success"),
+				},
+			},
+			expectedStatus: CIStatusPassed,
+			expectedAwait:  nil,
+		},
+		{
+			name:            "Single check - doesn't exist, status success, multiple statuses returned",
+			checksToLookFor: []string{"check1"},
+			mockRepoStatus: []*github.RepoStatus{
+				{
+					Context: github.String("what"),
+					State:   github.String("failed"),
+				},
+				{
+					Context: github.String("check1"),
+					State:   github.String("success"),
+				},
+			},
+			expectedStatus: CIStatusPassed,
+			expectedAwait:  nil,
+		},
+		{
+			name:            "Single check - doesn't exist, status failure",
+			checksToLookFor: []string{"check1"},
+			mockRepoStatus: []*github.RepoStatus{
+				{
+					Context: github.String("check1"),
+					State:   github.String("failure"),
+				},
+			},
+			expectedStatus: CIStatusFailed,
+			expectedAwait:  []string{"check1"},
+		},
+		{
+			name:            "Single check - doesn't exist, status pending",
+			checksToLookFor: []string{"check1"},
+			mockRepoStatus: []*github.RepoStatus{
+				{
+					Context: github.String("check1"),
+					State:   github.String("pending"),
+				},
+			},
+			expectedStatus: CIStatusPending,
+			expectedAwait:  []string{"check1"},
+		},
+		{
+			name:            "Single check - doesn't exist, status error",
+			checksToLookFor: []string{"check1"},
+			mockRepoStatus: []*github.RepoStatus{
+				{
+					Context: github.String("check1"),
+					State:   github.String("error"),
+				},
+			},
+			expectedStatus: CIStatusFailed,
+			expectedAwait:  []string{"check1"},
+		},
+		{
+			name:            "Multiple checks - all successful",
+			checksToLookFor: []string{"check1", "check2"},
+			mockCheckRuns: []*github.CheckRun{
+				{
+					Name:       github.String("check1"),
+					Status:     github.String("completed"),
+					Conclusion: github.String("success"),
+				},
+				{
+					Name:       github.String("check2"),
+					Status:     github.String("completed"),
+					Conclusion: github.String("success"),
+				},
+			},
+			expectedStatus: CIStatusPassed,
+			expectedAwait:  nil,
+		},
+		{
+			name:            "Multiple checks - one failed",
+			checksToLookFor: []string{"check1", "check2"},
+			mockCheckRuns: []*github.CheckRun{
+				{
+					Name:       github.String("check1"),
+					Status:     github.String("completed"),
+					Conclusion: github.String("success"),
+				},
+				{
+					Name:       github.String("check2"),
+					Status:     github.String("completed"),
+					Conclusion: github.String("failure"),
+				},
+			},
+			expectedStatus: CIStatusFailed,
+			expectedAwait:  []string{"check2"},
+		},
+		{
+			name:            "Multiple checks - one pending",
+			checksToLookFor: []string{"check1", "check2"},
+			mockCheckRuns: []*github.CheckRun{
+				{
+					Name:       github.String("check1"),
+					Status:     github.String("completed"),
+					Conclusion: github.String("success"),
+				},
+				{
+					Name:   github.String("check2"),
+					Status: github.String("queued"),
+				},
+			},
+			expectedStatus: CIStatusPending,
+			expectedAwait:  []string{"check2"},
+		},
+		{
+			name:            "Check not returned at all - unknown - converted to pending",
+			checksToLookFor: []string{"check1"},
+			expectedStatus:  CIStatusPending,
+			expectedAwait:   []string{"check1"},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			ctx := context.Background()
+
+			mockedHTTPClient := mock.NewMockedHTTPClient(
+				mock.WithRequestMatchHandler(
+					mock.GetReposCommitsCheckRunsByOwnerByRepoByRef,
+					http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+						// parse the request URI to get the check name. only
+						// return matching ones.
+						uri, err := url.ParseRequestURI(r.RequestURI)
+						require.NoError(t, err)
+						checkName := uri.Query().Get("check_name")
+
+						var runs []*github.CheckRun
+						if checkName == "" {
+							runs = tt.mockCheckRuns
+						} else {
+							for _, run := range tt.mockCheckRuns {
+								if *run.Name == checkName {
+									runs = append(runs, run)
+								}
+							}
+						}
+
+						_, err = w.Write(
+							mock.MustMarshal(
+								github.ListCheckRunsResults{
+									Total:     github.Int(len(runs)),
+									CheckRuns: runs,
+								},
+							),
+						)
+
+						require.NoError(t, err)
+					}),
+				),
+				mock.WithRequestMatch(
+					mock.GetReposCommitsStatusesByOwnerByRepoByRef,
+					tt.mockRepoStatus,
+				),
+			)
+
+			ghClient := newClientFromMock(t, mockedHTTPClient)
+			status, awaiting, err := ghClient.GetCIStatusForChecks(ctx, "owner", "repo", "abcdef12345", tt.checksToLookFor)
+
+			require.NoError(t, err)
+			require.Equalf(t, tt.expectedStatus, status, "expected status %s, got %s", tt.expectedStatus, status)
+			require.ElementsMatch(t, tt.expectedAwait, awaiting)
+		})
+	}
+}
+
+func TestGetCIStatusForChecks_Error(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+	ghClient := newErrorReturningClient(t)
+	_, _, err := ghClient.GetCIStatusForChecks(ctx, "owner", "repo", "abcdef12345", []string{"check1"})
+	require.Error(t, err)
+}
+
+// first call returns okay, second call returns error
+func TestGetCIStatusForChecks_ErrorListStatuses(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+
+	mockedHTTPClient := mock.NewMockedHTTPClient(
+		mock.WithRequestMatch(
+			mock.GetReposCommitsCheckRunsByOwnerByRepoByRef,
+			github.ListCheckRunsResults{
+				Total:     github.Int(0),
+				CheckRuns: []*github.CheckRun{},
+			},
+		),
+		errorReturningHandler(t, mock.GetReposCommitsStatusesByOwnerByRepoByRef),
+	)
+
+	ghClient := newClientFromMock(t, mockedHTTPClient)
+
+	_, _, err := ghClient.GetCIStatusForChecks(ctx, "owner", "repo", "abcdef12345", []string{"check1"})
+	require.Error(t, err)
+	require.ErrorContains(t, err, "failed to query GitHub")
 }
